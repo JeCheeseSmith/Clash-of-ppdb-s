a415afc7b511b284f1fa1c3d217db55e81d9728a
AbuRahman10 <abobaker.rahman10@gmail.com>
2024-02-21 21:05:44 +0100

    Made Directories for later use

--- src/frontend/src/App.jsx
+++ src/frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import gameBackground from './assets/gameBackground.png'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,25 +8,8 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+            <img src={gameBackground} alt=""/>
       </div>
-      <h1>Game Name</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Begin Template (Tijdelijk)
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
