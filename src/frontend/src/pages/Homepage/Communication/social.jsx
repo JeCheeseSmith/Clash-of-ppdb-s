b5ed332c9d3bba224c6c4080fae1258462cd86fe
AbuRahman10 <abobaker.rahman10@gmail.com>
2024-02-25 02:34:46 +0100

    Social Menu

--- src/frontend/src/pages/Homepage/Communication/social.jsx
+++ src/frontend/src/pages/Homepage/Communication/social.jsx
@@ -1,5 +1,6 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, {useState} from 'react';
 import "./social.css"
+import buttonSound from '../../../assets/Menu Selection Sound Effect.mp3';
 
 function SocialBox() {
   const [socialVisible, setSocialVisible] = useState(false);
@@ -7,13 +8,40 @@ function SocialBox() {
     setSocialVisible(!socialVisible);
   };
 
+  const playSound = () => {
+    const sound = new Audio(buttonSound);
+    sound.currentTime = 0.315;
+    sound.play();
+  };
+
+
   return (
       <div>
-        <button onClick={toggleSocialVisibility} className={`toggle-social-button ${socialVisible ? 'visible' : 'hidden'}`}>
-          {socialVisible ? 'social' : 'social'}
-        </button>
-        <div className={`social-container ${socialVisible ? 'visible' : 'hidden'}`}></div>
+          <button onClick={() => { toggleSocialVisibility(); playSound(); }} className={"toggle-social-button"}> social </button>
+          <div className="social-container"></div>
+          <Box socialVisible={socialVisible}/>
       </div>
   );
 }
 export default SocialBox;
+
+function Box({socialVisible}){
+    return(
+        <div className="box-container">
+            <Navbar socialVisible={socialVisible}/>
+        </div>
+    )
+}
+
+function Navbar({socialVisible}) {
+  return (
+    <nav className={`navbar ${socialVisible ? 'visible' : 'hidden'}`}>
+      <ul className="navbar-links">
+        <li><a href="#">Create Clan</a></li>
+        <li><a href="#">Join Clan</a></li>
+        <li><a href="#">Friend Requests</a></li>
+        <li><a href="#">Search Person</a></li>
+      </ul>
+    </nav>
+  );
+}
