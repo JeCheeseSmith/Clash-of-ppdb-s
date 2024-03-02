ddb9ad89bd468b289847eee476d6c3ad78c690e3
watson <kars@duck.com>
2024-03-02 17:56:36 +0100

    Backend cleanup

--- src/database.py
+++ src/database.py
@@ -1,13 +1,11 @@
-# Data Access Object pattern: see http://best-practice-software-engineering.ifs.tuwien.ac.at/patterns/dao.html
-# For clean separation of concerns, create separate data layer that abstracts all data access to/from RDBM
-
-# Depends on psycopg2 librarcy: see (tutor) https://wiki.postgresql.org/wiki/Using_psycopg2_with_PostgreSQL
 import psycopg2
 
+
 class DBConnection:
-    def __init__(self, dbname, dbuser, password):
+    def __init__(self):
         try:
-            self.conn = psycopg2.connect(user="postgres", password="password", host="127.0.0.1",port="5432",database="ppdb")
+            self.conn = psycopg2.connect(user="postgres", password="password", host="127.0.0.1", port="5432",
+                                         database="ppdb")
         except:
             print('ERROR: Unable to connect to database')
             raise
