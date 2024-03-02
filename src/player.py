f4d6b2dfce6ecfeeaa477664941c620e9dd85696
Salaheddine <sal.edfiri@gmail.com>
2024-03-02 14:08:07 +0100

    Testbranch to backendbranch

--- src/player.py
+++ src/player.py
@@ -1,5 +1,6 @@
 from database import *
 
+
 class Player:
     def __init__(self, name, password, avatar, gems, xp, level, logout):
         self.name = name
@@ -9,19 +10,32 @@ class Player:
         self.xp = xp
         self.level = level
         self.logout = logout
-        
+
     def to_dct(self):
-        return {'id': self.id, 'text': self.text, 'author': self.author}
+        return {'name': self.name, 'password': self.password}
 
 
 class PlayerDataAccess:
     def __init__(self, dbconnect):
         self.dbconnect = dbconnect
-       
-    def get_quotes(self):
+
+    def get_login(self, obj):
         cursor = self.dbconnect.get_cursor()
-        cursor.execute('SELECT * FROM player;')
-        temp = cursor.fetchall()
-        print(temp)
+        cursor.execute('SELECT * FROM player WHERE name=%s AND password=%s;', (obj.name, obj.password))
+        Controle = cursor.fetchone()
+        if Controle:
+            return True
+        else:
+            return False
+
+    def add_user(self, obj):
+        cursor = self.dbconnect.get_cursor()
+        try:
+            cursor.execute('INSERT INTO player(name,password) VALUES(%s,%s)', (obj.name, obj.password,))
+            self.dbconnect.commit()
+            return True
+        except:
+            self.dbconnect.rollback()
+            return False
 
 
