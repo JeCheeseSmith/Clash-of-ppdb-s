4b516b54e45c69bbbb2d9408b5ac8d2ed02f849f
AbuRahman10 <abobaker.rahman10@gmail.com>
2024-03-02 17:55:23 +0100

    Build Menu Button

--- src/frontend/src/pages/Homepage/mainpage.jsx
+++ src/frontend/src/pages/Homepage/mainpage.jsx
@@ -3,6 +3,7 @@ import './mainpage.css'; // Importing the CSS file for styling
 import Chat from './Communication/chat';
 import SocialBox from "./Communication/social.jsx";
 import MyThreeComponent from "./GridView/grid3D.jsx";
+import Buildmenu from "./BuildMenu/buildmenu.jsx";
 
 /**
  * Functional component representing the main page of the application.
@@ -13,6 +14,7 @@ function MainPage() {
       <div className="background"> {/* Container for the background image */}
           <Chat/>
           <SocialBox/>
+          <Buildmenu/>
           <MyThreeComponent/>
       </div>
   );
