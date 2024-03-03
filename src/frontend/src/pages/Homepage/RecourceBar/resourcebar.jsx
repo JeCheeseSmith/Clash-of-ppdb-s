90dd0034f727a1d4837b435e8e8119b67034be6f
raadin <bahramiraadin@gmail.com>
2024-03-03 15:39:39 +0100

    login + resource

---
+++ src/frontend/src/pages/Homepage/RecourceBar/resourcebar.jsx
@@ -0,0 +1,29 @@
+import React from 'react'; // Importing React library
+import './resourcebar.css'
+import woodLog from '/home/raadin/Documents/Clash-of-ppdb-s/src/frontend/src/assets/wood.png';
+import stone from '/home/raadin/Documents/Clash-of-ppdb-s/src/frontend/src/assets/stone.png';
+import metal from '/home/raadin/Documents/Clash-of-ppdb-s/src/frontend/src/assets/metal.png';
+import food from '/home/raadin/Documents/Clash-of-ppdb-s/src/frontend/src/assets/food.png';
+
+// Code for resourcebar
+function ResourceBar() {
+
+    return (
+        <div className="resourceBar">
+            <div className="resourceSection">
+                <img src={woodLog} alt="Wood" className="resourceIcon"/>
+            </div>
+            <div className="resourceSection">
+                <img src={stone} alt="Stone" className="resourceIcon"/>
+            </div>
+            <div className="resourceSection">
+                <img src={metal} alt="Metal" className="resourceIcon"/>
+            </div>
+            <div className="resourceSection">
+                <img src={food} alt="Food" className="resourceIcon"/>
+            </div>
+        </div>
+    );
+}
+
+export default ResourceBar;
