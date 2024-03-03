73d59a24e371e32b184a111aeb164cff0f54885d
watson <kars@duck.com>
2024-03-03 18:44:14 +0100

    tried doing updates

--- src/app.py
+++ src/app.py
@@ -2,19 +2,24 @@ from flask import Flask
 from flask import request, jsonify
 from src.dataAcces.player import *
 from src.dataAcces.message import *
-
 from flask.templating import render_template
+from database import *
+import dataAcces
+from flask import Flask, jsonify
+from flask_cors import CORS
 
 # INITIALIZE SINGLETON SERVICES
-app = Flask('Travisia',static_folder='frontend/dist/static',template_folder='frontend/dist')
-app_data = dict()
-app_data['app_name'] = 'Travisia'
+app = Flask('Travisia', static_folder='frontend/dist/static', template_folder='frontend/dist')
 connection = DBConnection()
 DEBUG = False
 HOST = "127.0.0.1" if DEBUG else "0.0.0.0"
+CORS(app)
 
-player_data_access = PlayerDataAccess(connection)
-Message_data_access = MessageDataAccess(connection)
+player_data_access = PlayerDataAccess(connection)  # Run on the same connection to minimise usage / # of connections
+message_data_access = MessageDataAccess(connection)
+
+
+# package_data_acces =
 
 
 @app.route('/Signin', methods=['POST'])
@@ -23,12 +28,13 @@ def add_player():
     player_name = data.get('name')
     player_password = data.get('password')
     controle = False
-    playerobj = Player(name=player_name, password=player_password, avatar=None, gems=None, xp=None, level=None,logout=None)
+    playerobj = Player(name=player_name, password=player_password, avatar=None, gems=None, xp=None, level=None,
+                       logout=None)
     controle = player_data_access.add_user(playerobj)
     if controle:
         return jsonify(playerobj.to_dct())
     else:
-        return "Failed signup"
+        return jsonify("Failed signup")
 
 
 @app.route('/login', methods=['GET'])
@@ -36,7 +42,8 @@ def get_login():
     data = request.json
     player_name = data.get('name')
     player_password = data.get('password')
-    playerobj = Player(name=player_name, password=player_password, avatar=None, gems=None, xp=None, level=None,logout=None)
+    playerobj = Player(name=player_name, password=player_password, avatar=None, gems=None, xp=None, level=None,
+                       logout=None)
     controle = False
     controle = player_data_access.get_login(playerobj)
 
@@ -57,27 +64,63 @@ def update_chatbox():
     if request.method == 'POST':
         controle = False
         chatobj = Message(message_id, message_moment, message_content, message_pname)
-        Rchatobj = Retrieve(message_id,message_sname)
-        controle = Message_data_access.add_message(chatobj)
+        Rchatobj = Retrieve(message_id, message_sname)
+        controle = message_data_access.add_message(chatobj)
         if controle == True:
-            controle = Message_data_access.add_message2(Rchatobj)
-            return jsonify(chatobj.to_dct(),Rchatobj.to_dct())
+            controle = message_data_access.add_message2(Rchatobj)
+            return jsonify(chatobj.to_dct(), Rchatobj.to_dct())
         else:
             return "Message failed to store"
 
-    elif request.method=='GET':
-        obj=Message_data_access.get_chatbox(message_pname)
+    elif request.method == 'GET':
+        obj = message_data_access.get_chatbox(message_pname)
         return jsonify(obj)
 
+
+@app.route('/resources/<int:id>', methods=['GET'])
+def get_resources():
+    """
+    Function to retrieve current amount of resources of a settlement
+    :return:
+    """
+    # User -> Settlement -> Package
+    # package
+
+    pass
+
+
+@app.route('/grid', methods=['GET'])
+def get_grid():
+    pass
+
+
+@app.route('/buildings', methods=['GET'])
+def get_buildings():
+    pass
+
+
+@app.route('/test')
+def test():
+    return jsonify({'kaas': "k"})
+
+@app.route('/test2', methods=['POST'])
+def test2():
+    data = request.json
+    print(data.get('username'))
+    #print(data.get('password'))
+    print('t')
+    return jsonify({'t': "t"})
 
 # -login
 # messages laatste tien en update naar mate aantal
 # clanrequest
 # clan aanmaken
+
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):
     return render_template("index.html")
+
 
 # RUN DEV SERVER
 if __name__ == "__main__":
