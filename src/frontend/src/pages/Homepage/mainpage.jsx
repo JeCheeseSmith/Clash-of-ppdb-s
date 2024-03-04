34e146779e29c99c14318b5a8ba65a1ed23e15e0
AbuRahman10 <abobaker.rahman10@gmail.com>
2024-03-04 17:50:02 +0100

    BuildMenu Basic Layout

--- src/frontend/src/pages/Homepage/mainpage.jsx
+++ src/frontend/src/pages/Homepage/mainpage.jsx
@@ -2,20 +2,21 @@ import React from 'react'; // Importing React library
 import './mainpage.css'; // Importing the CSS file for styling
 import Chat from './Communication/chat';
 import SocialBox from "./Communication/social.jsx";
-import MyThreeComponent from "./GridView/grid3D.jsx";
+import Grid from "./GridView/grid3D.jsx";
 import Buildmenu from "./BuildMenu/buildmenu.jsx";
 
 /**
  * Functional component representing the main page of the application.
  * Displays a full-bleed background image.
  */
-function MainPage() {
+function MainPage()
+{
   return (
       <div className="background"> {/* Container for the background image */}
           <Chat/>
           <SocialBox/>
           <Buildmenu/>
-          <MyThreeComponent/>
+          <Grid/>
       </div>
   );
 }
