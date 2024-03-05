88161f9d52ce6d06d52f375bf8709d7bfd92add8
raadin <bahramiraadin@gmail.com>
2024-03-05 12:39:06 +0100

    resourcebar aangepast

--- src/frontend/src/pages/Homepage/RecourceBar/resourcebar.jsx
+++ src/frontend/src/pages/Homepage/RecourceBar/resourcebar.jsx
@@ -5,26 +5,28 @@ import stone from '/home/raadin/Documents/Clash-of-ppdb-s/src/frontend/src/asset
 import metal from '/home/raadin/Documents/Clash-of-ppdb-s/src/frontend/src/assets/metal.png';
 import food from '/home/raadin/Documents/Clash-of-ppdb-s/src/frontend/src/assets/food.png';
 
-// Code for resourcebar
+// Code for resource-bar
 function ResourceBar() {
 
     return (
+        // Resource-bar section
         <div className="resourceBar">
+            {/* Each resource has a symbol and a count */}
             <div className="resourceSection">
                 <img src={woodLog} alt="Wood" className="resourceIcon"/>
-                <span className="resourceCount">5000</span> {/* Dit is het nieuwe element */}
+                <span className="resourceCount">5000</span>
             </div>
             <div className="resourceSection">
                 <img src={stone} alt="Stone" className="resourceIcon"/>
-                <span className="resourceCount">5000</span> {/* Dit is het nieuwe element */}
+                <span className="resourceCount">5000</span>
             </div>
             <div className="resourceSection">
                 <img src={metal} alt="Metal" className="resourceIcon"/>
-                <span className="resourceCount">5000</span> {/* Dit is het nieuwe element */}
+                <span className="resourceCount">5000</span>
             </div>
             <div className="resourceSection">
                 <img src={food} alt="Food" className="resourceIcon"/>
-                <span className="resourceCount">5000</span> {/* Dit is het nieuwe element */}
+                <span className="resourceCount">5000</span>
             </div>
         </div>
     );
