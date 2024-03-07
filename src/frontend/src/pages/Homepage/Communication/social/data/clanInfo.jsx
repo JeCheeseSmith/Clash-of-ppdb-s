90ebea09cc588a8175cbf0f62d5a02b4a655c6dd
AbuRahman10 <abobaker.rahman10@gmail.com>
2024-03-07 22:03:37 +0100

    Documented clanInfo

--- src/frontend/src/pages/Homepage/Communication/social/data/clanInfo.jsx
+++ src/frontend/src/pages/Homepage/Communication/social/data/clanInfo.jsx
@@ -2,6 +2,17 @@ import React, {useState} from 'react';
 import "./clanInfo.css"
 import clanPicture from "../../../../../assets/clanPicture.jpg";
 
+/**
+ * Represents a component for displaying information about a clan.
+ * @param {Object} props - The props passed to the component.
+ * @param {string} props.name - The name of the clan.
+ * @param {string} props.description - Description or information about the clan.
+ * @param {string} props.status - Status of the clan.
+ * @param {string} props.pname - The name of the clan leader.
+ * @param {string} props.success - Success: Clan exists or not.
+ * @returns {JSX.Element} - A React JSX element representing the clan information component.
+ */
+
 function ClanInformation({name, description, status, pname, success}) {
     return(
         <div className={"clan-info-button"}>
