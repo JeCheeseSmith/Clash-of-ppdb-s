ca506a55aaf39f782dcbaad637e3ad6ed696776f
raadin <bahramiraadin@gmail.com>
2024-03-13 13:39:40 +0100

    login met error

--- src/frontend/src/pages/Loginpage/Login/login.jsx
+++ src/frontend/src/pages/Loginpage/Login/login.jsx
@@ -2,6 +2,7 @@ import React, { useState} from 'react'; // Importing React library
 import './login_signup.css';
 import { useNavigate } from 'react-router-dom';
 import POST from "../../../api/POST.jsx";
+import { AiOutlineExclamationCircle } from 'react-icons/ai'; // Zorg dat je react-icons hebt geïnstalleerd
 
 // Code for login page
 function LoginPage() {
@@ -9,6 +10,7 @@ function LoginPage() {
     // State for username & password
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [errormessage, setErrorMessage] = useState('');
 
     // Handler for username change
     function handleUsernameChange(event) {
@@ -28,11 +30,13 @@ function LoginPage() {
 
     // Handles the navigation from login page to mainpage
     const handleLoginClick = async () => {
-        const data = await POST({name:username, password: password}, "/login");
-        console.log(data);
-        if (data.success) {
-            navigate('/MainPage', { state: { username } });
-        }
+      const data = await POST({ name: username, password: password }, "/login");
+      if (data.success) {
+        navigate('/MainPage', { state: { username } });
+      }
+      else {
+        setErrorMessage('Verkeerde login details');
+      }
     }
 
   return (
@@ -41,6 +45,11 @@ function LoginPage() {
           <h1 className="gametitle">TRAVISIA</h1>
           <h2 className="subtitle">FALLEN EMPIRE</h2>
           <div className="login-form">
+              {errormessage && (
+          <div className="error-message">
+            <AiOutlineExclamationCircle /> {errormessage}
+          </div>
+        )}
               <div>
                   {/* <div> groupes the label and input together on one line */}
                   <label htmlFor="username">Username:</label>
