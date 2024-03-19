5c74543cd1c8d82659b81ff2afd4607d1bd8a25f
AbuRahman10 <abobaker.rahman10@gmail.com>
2024-03-19 20:03:21 +0100

    Update Objects

--- src/frontend/src/pages/Homepage/GridBuilder/GridView/grid3D.jsx
+++ src/frontend/src/pages/Homepage/GridBuilder/GridView/grid3D.jsx
@@ -1,4 +1,4 @@
-import React, {Suspense, useEffect, useState} from 'react';
+import React, {Suspense} from 'react';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
 import './grid3D.css'
