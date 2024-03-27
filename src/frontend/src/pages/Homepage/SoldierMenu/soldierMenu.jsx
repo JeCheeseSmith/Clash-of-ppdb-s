5ac01625ffca57b76d5270c6ec0d8fd9233846d8
JonasDegruytere <125671105+JonasDegruytere@users.noreply.github.com>
2024-03-27 11:44:01 +0100

    Semi functional prototype Soldiermenu

--- src/frontend/src/pages/Homepage/SoldierMenu/soldierMenu.jsx
+++ src/frontend/src/pages/Homepage/SoldierMenu/soldierMenu.jsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import './soldierMenu.css'
 import CommunicationButton from "../Communication/communication.jsx";
+import SocialOption from "../Communication/social/socialOptionContents.jsx";
 
 /**
  * React component for the troop screen button
@@ -13,15 +14,74 @@ function SoldierMenuButton(props) {
                 setsoldierVisible(!soldierVisible);
             }} className={"trainMenu"}>Troop Menu
             </button>
-            {soldierVisible? SoldierMenuBox():null}
+            {soldierVisible? SoldierMenuBox(soldierVisible):null}
         </>
     );
 }
 export default SoldierMenuButton;
 
-function SoldierMenuBox() {
-    return(
+function SoldierMenuBox(soldierVisible) {
+    return (
         <div className="SoldierMenubox">
+            <SoldierNavbar soldierVisible={soldierVisible}/>
+        </div>
+    )
+}
+
+function SoldierNavbar(soldierVisible) {
+    const [currentPage, setCurrentPage] = useState('troopoverview');
+
+    const handleButtonClick = (pageName) => {
+      setCurrentPage(pageName);
+      playOption();
+    };
+
+    return (
+        <div className="SoldierMenubox">
+            {soldierVisible && (
+            <nav className="navbar visible">
+                <ul className="navbar-links">
+                    <li>
+                        <button onClick={() => handleButtonClick('troopoverview')} className={"SoldierMenubox"}>Troop Overview
+                        </button>
+                    </li>
+                    <li>
+                        <button onClick={() => handleButtonClick('traintroopoverview')} className={"SoldierMenubox"}>Troop Training
+                        </button>
+                    </li>
+                </ul>
+            </nav>
+        )}
+        {
+            soldierVisible && currentPage &&
+            (<SoldierMenuOptions pageName={currentPage}/>)
+        }
+        </div>
+    )
+}
+
+function SoldierMenuOptions({pageName, requests, sendData}){
+    return (
+        <div className="page-content">
+            {pageName === 'troopoverview' && <troopoverviewPage/>}
+            {pageName === 'traintroop' && <traintroopPage/>}
+        </div>
+    )
+}
+
+
+function troopoverviewPage() {
+    return (
+        <div className="troopoverview">
+
+        </div>
+    )
+}
+
+function trooptrainPage() {
+    return (
+        <div className="traintroopview">
+
         </div>
     )
 }
