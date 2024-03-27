fa8c6674de32d4eb442318fd9d57bb58ea5c4640
raadin <bahramiraadin@gmail.com>
2024-03-27 21:53:05 +0100

    progress bar

--- src/frontend/src/pages/Homepage/GridBuilder/GridView/upgradeBuilding/upgradeBuilding.jsx
+++ src/frontend/src/pages/Homepage/GridBuilder/GridView/upgradeBuilding/upgradeBuilding.jsx
@@ -1,16 +1,25 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import './upgradeBuilding.css'
-import '../../../../../globalComponents/timeComponent'
+import '../../../../../globalComponents/timeComponent/TimerProgressBar.jsx'
+import TimerProgressBar from "../../../../../globalComponents/timeComponent/TimerProgressBar.jsx";
 
 function UpgradeBuilding() {
 
-    function UpgradeBuilding() {
+    const [time, setTime] = useState(null);
+    const [click, setClick] = useState(false);
 
+    async function HandleUpgradeClick() {
+        //const data = await GET({}, '/update');
+        if (true) {
+            setClick(true);
+            setTime(20);
+        }
     }
 
     return (
         <div className="button-container">
-            <button className="upgrade-button" onClick={UpgradeBuilding}> Upgrade</button>
+            {click && <TimerProgressBar timeValue={time} finished={setClick}/>}
+            {!click && <button className="upgrade-button" onClick={HandleUpgradeClick}> Upgrade</button>}
         </div>
     );
 }
