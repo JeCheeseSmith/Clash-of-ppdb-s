19380b40f22124ccc354ab4d8cbdfd63020b3f34
JonasDegruytere <125671105+JonasDegruytere@users.noreply.github.com>
2024-04-01 22:08:34 +0200

    textures

--- src/frontend/src/pages/Homepage/SoldierMenu/soldierMenu.jsx
+++ src/frontend/src/pages/Homepage/SoldierMenu/soldierMenu.jsx
@@ -1,6 +1,20 @@
 import React, {useState, useEffect} from 'react';
 import './soldierMenu.css'
-import warrior from "./assets/knight.png";
+import heavyInfantry3 from "./assets/templar.png";
+import heavyInfantry2 from "./assets/knight.png";
+import heavyInfantry1 from "./assets/swordman.png"
+import horseman1 from "./assets/Horseman.png"
+import horseman2 from "./assets/Knight.png"
+import horseman3 from "./assets/War elephant.png"
+import bowman1 from "./assets/bowman.png"
+import bowman2 from "./assets/Longbowman.png"
+import bowman3 from "./assets/Crossbowman.png"
+import ambush1 from "./assets/bandit.png"
+import ambush2 from "./assets/militia.png"
+import ambush3 from "./assets/assassin.png"
+import spear1 from "./assets/guardian.png"
+import spear2 from "./assets/Pikeman.png"
+import spear3 from "./assets/Halbardier.png"
 
 /**
  * React component for the troop screen button
@@ -82,27 +96,43 @@ function TroopOverviewPage() {
             <div className="soldier-primair-input">
                 {/* Each soldier has a symbol and a count */}
                 <div className="soldierSection">
-                    <img src={warrior} alt="Armored footman" className="soldier-icon"/>
+                    <img src={heavyInfantry1} alt="Armored footman" className="soldier-icon"/>
                     <div className="soldierCount">1</div>
-                    <img src={warrior} alt="Huskarl" className="soldier-icon"/>
+                    <img src={heavyInfantry2} alt="Huskarl" className="soldier-icon"/>
                     <div className="soldierCount">1</div>
-                    <img src={warrior} alt="Order Knights" className="soldier-icon"/>
+                    <img src={heavyInfantry3} alt="Order Knights" className="soldier-icon"/>
                     <div className="soldierCount">1</div>
                 </div>
                 <div className="soldierSection">
-                    <img src={warrior} alt="test" className="soldier-icon"/>
+                    <img src={spear1} alt="Guardsman" className="soldier-icon"/>
                     <div className="soldierCount">0</div>
-                    <img src={warrior} alt="test" className="soldier-icon"/>
+                    <img src={spear2} alt="Pike man" className="soldier-icon"/>
                     <div className="soldierCount">0</div>
-                    <img src={warrior} alt="test" className="soldier-icon"/>
+                    <img src={spear3} alt="Halbardier" className="soldier-icon"/>
                     <div className="soldierCount">0</div>
                 </div>
                 <div className="soldierSection">
-                    <img src={warrior} alt="test 2" className="soldier-icon"/>
-                    <div className="soldierCount">2</div>
-                    <img src={warrior} alt="test 2" className="soldier-icon"/>
+                    <img src={horseman1} alt="Horseman" className="soldier-icon"/>
                     <div className="soldierCount">0</div>
-                    <img src={warrior} alt="test 2" className="soldier-icon"/>
+                    <img src={horseman2} alt="Knight" className="soldier-icon"/>
+                    <div className="soldierCount">0</div>
+                    <img src={horseman3} alt="War elephant" className="soldier-icon"/>
+                    <div className="soldierCount">0</div>
+                </div>
+                <div className="soldierSection">
+                    <img src={bowman1} alt="Bowman" className="soldier-icon"/>
+                    <div className="soldierCount">0</div>
+                    <img src={bowman2} alt="Longbowman" className="soldier-icon"/>
+                    <div className="soldierCount">0</div>
+                    <img src={bowman3} alt="Crossbowman" className="soldier-icon"/>
+                    <div className="soldierCount">0</div>
+                </div>
+                <div className="soldierSection">
+                    <img src={ambush1} alt="Bandit" className="soldier-icon"/>
+                    <div className="soldierCount">0</div>
+                    <img src={ambush2} alt="Militia" className="soldier-icon"/>
+                    <div className="soldierCount">0</div>
+                    <img src={ambush3} alt="Skirmishers" className="soldier-icon"/>
                     <div className="soldierCount">0</div>
                 </div>
             </div>
