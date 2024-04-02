231bf07d94095483e28f590525a83dc611a74bbd
JonasDegruytere <125671105+JonasDegruytere@users.noreply.github.com>
2024-04-02 12:54:28 +0200

    basic graphics for trainingqueue

--- src/frontend/src/pages/Homepage/SoldierMenu/soldierMenu.jsx
+++ src/frontend/src/pages/Homepage/SoldierMenu/soldierMenu.jsx
@@ -144,6 +144,7 @@ function TroopTrainPage() {
     return (
         <div className="soldier-primair-input">
             <div className="army-title"> Training Queue</div>
+            <div className="trainingQueue"></div>
         </div>
     )
 }
