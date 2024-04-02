cdbffd702f42abb71f70822529f6179aef8ce1fe
raadin <bahramiraadin@gmail.com>
2024-04-02 11:57:59 +0200

    upgrade uitgebreid

--- src/frontend/src/pages/Homepage/GridBuilder/GridView/upgradeBuilding/upgradeBuilding.jsx
+++ src/frontend/src/pages/Homepage/GridBuilder/GridView/upgradeBuilding/upgradeBuilding.jsx
@@ -3,11 +3,14 @@ import './upgradeBuilding.css'
 import '../../../../../globalComponents/timeComponent/TimerProgressBar.jsx'
 import TimerProgressBar from "../../../../../globalComponents/timeComponent/TimerProgressBar.jsx";
 import DisplayAvatarName from "../../../../../globalComponents/avatarWithName/avatarWithName.jsx";
+import { AiOutlineExclamationCircle } from 'react-icons/ai';
+
 
 function UpgradeBuilding({selectedBuilding}) {
 
     const [time, setTime] = useState(null);
     const [click, setClick] = useState(false);
+    const [errormessage, setErrorMessage] = useState(null);
 
     async function HandleUpgradeClick() {
         //const data = await GET({}, '/update');
@@ -15,11 +18,15 @@ function UpgradeBuilding({selectedBuilding}) {
             setClick(true);
             setTime(20);
         }
+        else {
+            setErrorMessage('Not enough resources');
+        }
     }
 
     return (
         <div className="button-container">
             <DisplayAvatarName type={"building-selected"} name={selectedBuilding[0].type}/>
+            {errormessage && (<div className="error-message"> <AiOutlineExclamationCircle /> {errormessage} </div>)}
             {click && <TimerProgressBar timeValue={time} finished={setClick}/>}
             {!click && <button className="upgrade-button" onClick={HandleUpgradeClick}> Upgrade</button>}
         </div>
