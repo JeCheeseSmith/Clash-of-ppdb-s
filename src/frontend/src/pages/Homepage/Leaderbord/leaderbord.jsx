756e1de06d6dc06eea2d6fd7ed32795f802bc35f
Salaheddine <sal.edfiri@gmail.com>
2024-04-13 15:27:21 +0200

    Leaderbord update

--- src/frontend/src/pages/Homepage/Leaderbord/leaderbord.jsx
+++ src/frontend/src/pages/Homepage/Leaderbord/leaderbord.jsx
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './leaderbord.css'
 
-function leaderbord() {
-    return (
-        <button className={"toggle-leaderbord-button"}>leaderbord</button>
+
+function Leaderboard() {
+  // State to manage the visibility of the menu
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  // Function to toggle the menu
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  return (
+      <div>
+            <button onClick={toggleMenu} className="toggle-leaderbord-button">Leaderboard</button>
+          {isMenuOpen && (
+            <div className={`box-container ${isMenuOpen  ? 'visible' : 'hidden'}`}>
+                <h1 className="chat-title">CHAT</h1>
+                <ul>
+                    <li>Menu Item 1</li>
+                    <li>Menu Item 2</li>
+                    <li>Menu Item 3</li>
+                </ul>
+            </div>
+               )}
+        </div>
     );
 }
 
-export default leaderbord;
+
+export default Leaderboard;
