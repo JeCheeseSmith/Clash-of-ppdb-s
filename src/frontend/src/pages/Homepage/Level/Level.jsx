c046085c64f5819fe7fe46313335dc22d266c805
raadin <bahramiraadin@gmail.com>
2024-04-14 14:12:59 +0200

    level with comments

--- src/frontend/src/pages/Homepage/Level/Level.jsx
+++ src/frontend/src/pages/Homepage/Level/Level.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import './level.css'
 
+// Code for level
 function LevelBar() {
 
+    // This variable represents the amount of xp you have
     const xpPercentage = 50;
 
     return (
         <div className="level-xp-container">
-            <div className="level-container">
-                <div className="level-count">1</div>
-            </div>
+            <div className="level-container">1</div>
             <div className="xp-bar-container">
                 <div className="xp-bar" style={{width: `${xpPercentage}%`}}></div>
             </div>
