dc7846e3b02fcf93365ccca07ab493edad16ae8e
Salaheddine <sal.edfiri@gmail.com>
2024-04-15 22:42:53 +0200

    Documentation for wheel and leaderboard

--- src/frontend/src/pages/Homepage/Leaderbord/leaderboard.jsx
+++ src/frontend/src/pages/Homepage/Leaderbord/leaderboard.jsx
@@ -3,27 +3,32 @@ import './leaderboard.css'
 
 
 function Leaderboard() {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
-  };
+    // Variable to check if the toggle-leaderboard-button has been clicked, initially set to false
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  return (
-      <div>
+    // This function handles the toggle-leaderboard-button click
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
+    return (
+        <div>
             <button onClick={toggleMenu} className="toggle-leaderboard-button"></button>
-          {isMenuOpen && (
-            <div className={`leaderboard-container ${isMenuOpen  ? 'visible' : 'hidden'}`}>
-                <div className="leaderboardmenu">
-                    <h1 className="leaderboard-title">Leaderboard</h1>
-                    <ul>
-                        <li>Player 1</li>
-                        <li>player 2</li>
-                        <li>Player 3</li>
-                    </ul>
+             {/* If click is true (toggle-leaderboard-button has been clicked), then the leaderboard will open up */}
+            {isMenuOpen && (
+                <div className={`leaderboard-container ${isMenuOpen  ? 'visible' : 'hidden'}`}>
+                    <div className="leaderboardmenu">
+                        <h1 className="leaderboard-title">Leaderboard</h1>
+                         {/* just hardcoded for the demo but it will be connected with an api */}
+                        <ul>
+                            <li>Player 1</li>
+                            <li>player 2</li>
+                            <li>Player 3</li>
+                        </ul>
+                    </div>
                 </div>
-            </div>
-            )}
+                )}
         </div>
     );
 }
--- src/frontend/src/pages/Homepage/Wheel of fortune /wheel.jsx
+++ src/frontend/src/pages/Homepage/Wheel of fortune /wheel.jsx
@@ -1,31 +1,42 @@
 import React, {useState} from 'react';
 import './wheel.css'
 
-
-
-
 const WheelOfFortune = () => {
+
+  // Variable to check if the toggle-leaderboard-button has been clicked, initially set to false
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  // Variable to set the rotation, initially set to 0
   const [rotation, setRotation] = useState(0);
+  // Variable to set the popup(prize) true or false, initially set to false
   const [showPrize, setShowPrize] = useState(false);
+  // Variable to set the prize itself, initially an empty string
   const [prize, setPrize] = useState('');
 
+  // This function handles the toggle-wheel-button click
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
+  // This is made for the spin of the wheel
   const spinWheel = () => {
+    // First part is to calculate the rotation of the wheel
     const newValue = Math.ceil(Math.random() * 3600);
     const newRotation = rotation + newValue;
     setRotation(newRotation);
     setShowPrize(false);
+
+    // Set a timer when to show the popup
     setTimeout(() => {
     setIsMenuOpen(false);
     setShowPrize(true);
     }, 6000);
+
+    // Set a timer when to close the popup
     setTimeout(() => {
      setShowPrize(false);
     }, 7000);
+
+    // Set a timer of the wheel itself after it gets the price
     setTimeout(() => {
       const arrowAngle = (390 - (newRotation % 360)) % 360;
       const newPrize = getPrize(arrowAngle);
@@ -33,13 +44,13 @@ const WheelOfFortune = () => {
     },5000);
 };
 
-
+  // Function to calculate the prize with an angle and with the given segments we used in the wheel
   const getPrize = (angle) => {
-    const segmentAngle = 360 / 10;
-    let prize = '';
+    const segmentAngle=360/10;
+    let prize='';
 
-    if (angle >= 0 && angle < segmentAngle) {
-      prize = 'You won 1000 wood!';
+    if(angle>=0&&angle<segmentAngle){
+      prize='You won 1000 wood!';
     } else if (angle >= segmentAngle && angle < 2 * segmentAngle) {
       prize = 'You won 1000 stone!';
     } else if (angle >= 2 * segmentAngle && angle < 3 * segmentAngle) {
@@ -55,8 +66,8 @@ const WheelOfFortune = () => {
     } else if (angle >= 7 * segmentAngle && angle < 8 * segmentAngle) {
       prize = 'You won Free soldier!';
     } else if (angle >= 8 * segmentAngle && angle < 9 * segmentAngle) {
-      prize = 'You won Level up!';
-    } else {
+      prize='You won Level up!';
+    }else {
       prize = 'You won 2000 gems!';
     }
     return prize;
@@ -65,10 +76,13 @@ const WheelOfFortune = () => {
   return (
       <div>
         <button onClick={toggleMenu} className="toggle-wheel-button"></button>
+        {/* If click is true (toggle-wheel-button has been clicked), then the leaderboard will open up */}
         {isMenuOpen && (
             <div className={"spin-container"}>
               <div className={"spin-button"} onClick={spinWheel}>Spin</div>
+              {/* So here when spin-button is clicked the wheel will spin and update the rotation */}
               <div className={"wheel-container"} style={{ transform: `rotate(${rotation}deg)` }}>
+                {/* Wheel is divided in ten segments with ten prizes to win (every segment has it color) */}
                 <div className={"segments-wheel"} style={{ "--i": 1, "--clr": "#ff0000" }}><span>1000 wood</span></div>
                 <div className={"segments-wheel"} style={{ "--i": 2, "--clr": "#ffff00" }}><span>1000 stone</span></div>
                 <div className={"segments-wheel"} style={{ "--i": 3, "--clr": "#0000FF" }}><span>1000 food</span></div>
@@ -82,6 +96,7 @@ const WheelOfFortune = () => {
               </div>
             </div>
         )}
+        {/* So when the spin is finished with spinning then the spin wil go away and the prize will be show as a popup */}
         {showPrize && (
         <div className="prize">
           <div className="prize-content">
@@ -92,8 +107,6 @@ const WheelOfFortune = () => {
       </div>
   );
 };
-
-
 
 export default WheelOfFortune;
 
