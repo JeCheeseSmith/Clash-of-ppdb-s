44bdfa52655740809f3671fc6c8a9a23e5175a35
Abobaker Rahman <abobaker.rahman10@gmail.com>
2024-05-18 15:23:05 +0200

    Fix

--- src/frontend/src/globalComponents/developersInfo/information.jsx
+++ src/frontend/src/globalComponents/developersInfo/information.jsx
@@ -3,31 +3,6 @@ import './Information.css'
 
 function Information()
 {
-    // Automatically updating last update if we have a public repo
-
-    /*const [commits, setCommits] = useState([]);
-    // Replace 'YOUR_ACCESS_TOKEN' with our GitHub access token
-    const accessToken = 'YOUR_ACCESS_TOKEN';
-    useEffect(() => {
-        fetch("https://api.github.com/repos/JeCheeseSmith/Clash-of-ppdb-s/commits", {
-            headers: {
-                Authorization: `token ${accessToken}`
-            }
-        })
-        .then((response) => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        })
-        .then((data) => {
-            setCommits(data);
-            console.log(data);
-        })
-        .catch((error) => {
-            console.error('Error fetching data:', error);
-        });
-    }, []);*/
     return (
         <>
             <div className={"last-update"}>
