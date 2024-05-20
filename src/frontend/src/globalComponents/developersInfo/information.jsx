1d96e5743a01e58fa4b8d0231078d97b45a10a61
Abobaker Rahman <abobaker.rahman10@gmail.com>
2024-05-20 13:35:16 +0200

    Update

--- src/frontend/src/globalComponents/developersInfo/information.jsx
+++ src/frontend/src/globalComponents/developersInfo/information.jsx
@@ -17,7 +17,7 @@ function Information()
     return (
         <>
             <div className={"last-update"}>
-                Last Update: 20/05/2024 00:23
+                Last Update: 20/05/2024 13:35
             </div>
             <div className={"feedback"}>
                 This Game is still in development, so if you encounter any problems or
